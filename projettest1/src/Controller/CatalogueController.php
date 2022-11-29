adb8630b1ed98bb26e9a14232d586a08ec9c4858
arezkidev1 <arezkidev@gmail.com>
2022-11-29 16:36:46 +0100

    29112022_16h30

--- projettest1/src/Controller/CatalogueController.php
+++ projettest1/src/Controller/CatalogueController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ProduitRepository;
 use App\Repository\CategorieRepository;
+use App\Entity\Categorie;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,19 +22,19 @@ class CatalogueController extends AbstractController
     }
 
 
-    #[Route('/souscategorie', name: 'app_souscategorie')]
-    public function souscategorie(CategorieRepository $categorieRepository): Response
+    #[Route('/souscategorie/{categorie}', name: 'app_souscategorie')]
+    public function souscategorie(Categorie $categorie): Response
     {
         return $this->render('catalogue/souscategorie.html.twig', [
-            'categories' => $categorieRepository->findAll(),
+            'categories' => $categorie->getCategories()
         ]);
     }
 
-    #[Route('/produit', name: 'app_produit')]
-    public function produit(ProduitRepository $produitRepository): Response
+    #[Route('/produit/{categorie}', name: 'app_produit')]
+    public function produit(Categorie $categorie): Response
     {
         return $this->render('catalogue/produit.html.twig', [
-            'produits' => $produitRepository->findAll(),
+            'produits' => $categorie->getProduits(),
         ]);
     }
 
--- projettest1/src/DataFixtures/Jeu1.php
+++ projettest1/src/DataFixtures/Jeu1.php
@@ -36,39 +36,87 @@ class Jeu1 extends Fixture
 
         $c2sc1 = new Categorie();
         $c2sc1->setName("Clavier Acoustique");
-        $c2sc1->setParentCategorie($c1);
+        $c2sc1->setParentCategorie($c2);
         $manager->persist($c2sc1);
 
         $c2sc2 = new Categorie();
         $c2sc2->setName("Clavier Electrique");
-        $c2sc2->setParentCategorie($c1);
+        $c2sc2->setParentCategorie($c2);
         $manager->persist($c2sc2);
+
+
+
+
+        for ($i = 0; $i < 20; $i++) {
+            $produit = new Produit();
+            $produit->setName("Guitares" . $i)
+                ->setDescription("jolie guitare")
+                ->setPrix(mt_rand(10, 250))
+                ->setCategorie($c1sc2);
+            $manager->persist($produit);
+        }
+
 
 
 
         $p1 = new Produit();
         $p1->setName("Guitares");
         $p1->setDescription("jolie guitare");
+        $p1->setPrix(111);
         $p1->setCategorie($c1sc2);
         $manager->persist($p1);
+
 
         $p2 = new Produit();
         $p2->setName("Guitares");
         $p2->setDescription("vieille guitare");
+        $p2->setPrix(222);
         $p2->setCategorie($c1sc1);
         $manager->persist($p2);
+
+        for ($i = 0; $i < 20; $i++) {
+            $produit = new Produit();
+            $produit->setName("Guitares" . $i)
+                ->setDescription("vieille guitare")
+                ->setPrix(mt_rand(10, 250))
+                ->setCategorie($c1sc1);
+            $manager->persist($produit);
+        }
+
 
         $p3 = new Produit();
         $p3->setName("Piano");
         $p3->setDescription("joli piano");
+        $p3->setPrix(333);
         $p3->setCategorie($c2sc1);
         $manager->persist($p3);
+
+        for ($i = 0; $i < 20; $i++) {
+            $produit = new Produit();
+            $produit->setName("Guitares" . $i)
+                ->setDescription("joli piano")
+                ->setPrix(mt_rand(10, 250))
+                ->setCategorie($c2sc1);
+            $manager->persist($produit);
+        }
+
 
         $p4 = new Produit();
         $p4->setName("Clavier");
         $p4->setDescription("joli clavier");
+        $p4->setPrix(444);
         $p4->setCategorie($c2sc2);
         $manager->persist($p4);
+
+        for ($i = 0; $i < 20; $i++) {
+            $produit = new Produit();
+            $produit->setName("Guitares" . $i)
+                ->setDescription("joli clavier")
+                ->setPrix(mt_rand(10, 250))
+                ->setCategorie($c2sc2);
+            $manager->persist($produit);
+        }
+
 
 
 
