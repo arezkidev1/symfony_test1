b0d13d819ed753b0fcfd4fa81912b9c59c8b4634
arezkidev1 <arezkidev@gmail.com>
2022-12-20 09:27:17 +0100

    20122022_9h30

---
+++ projettest1/src/Controller/CommandeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommandeController extends AbstractController
+{
+   /*  #[Route('/commande', name: 'app_commande')]
+    public function index(): Response
+    {
+        return $this->render('commande/index.html.twig', [
+            'controller_name' => 'CommandeController',
+        ]);
+    }
+ */
+    #[Route('/commande', name: 'app_commande')]
+    public function commande(): Response
+    {
+        return $this->render('commande/commande.html.twig', [
+            'controller_name' => 'CommandeController',
+        ]);
+    }
+
+}
